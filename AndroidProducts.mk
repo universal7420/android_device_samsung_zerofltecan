4255ef459991d26fd3e088fca5a89026e31b28c3
Lukas Berger <mail@lukasberger.at>
2017-06-14 22:36:03 +0200

    zero: create unified device-tree for Lineage/AOKP
    Change-Id: I0a316b4f3106a3a470a936c9d96387946228e93f

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,10 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_zerofltecan.mk \
+    $(LOCAL_DIR)/full_zerofltecan.mk
+
+ifneq ($(AOKP_VERSION),)
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/aokp.mk
+else
+PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lineage.mk
+endif
