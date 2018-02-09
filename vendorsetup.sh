8bd65e80000a99dcbe66451ed3ba56ba8fb8740d
Lukas0610 <mail@lukasberger.at>
2018-02-09 18:04:21 +0100

    zerofltecan: added device-makefiles for ResurrectionRemix
    Change-Id: I898da0e08c6211850a7df1b18f5b6e6dc9064b3c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,3 +20,4 @@ add_lunch_combo gzosp_zerofltecan-userdebug
 add_lunch_combo lineage_zerofltecan-userdebug
 add_lunch_combo nexus_zerofltecan-userdebug
 add_lunch_combo omni_zerofltecan-userdebug
+add_lunch_combo rr_zerofltecan-userdebug
